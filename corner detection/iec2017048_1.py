84c91105a3a240cf6e3a89204ef672d996b25389
ashraysinha6069 <iec2017048@iiita.ac.in>
2020-06-20 00:54:06 +0530

    corner detection

---
+++ corner detection/iec2017048_1.py
@@ -0,0 +1,62 @@
+from skimage.io import imread
+from skimage.color import rgb2gray
+from scipy import signal as sig
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy import ndimage as ndi
+import cv2
+
+
+img = cv2.imread('input.jpg')
+imggray = rgb2gray(img)
+height = len(imggray)
+width = len(imggray[0])
+offset = 50
+
+def gradient_x(imggray):
+    ##Sobel operator kernels.
+    kernel_x = np.array([[-1, 0, 1],[-2, 0, 2],[-1, 0, 1]])
+    return sig.convolve2d(imggray, kernel_x, mode='same')
+
+def gradient_y(imggray):
+    kernel_y = np.array([[1, 2, 1], [0, 0, 0], [-1, -2, -1]])
+    return sig.convolve2d(imggray, kernel_y, mode='same')
+
+I_x = gradient_x(imggray)
+I_y = gradient_y(imggray)
+
+Ixx = ndi.gaussian_filter(I_x**2, sigma=1)
+Ixy = ndi.gaussian_filter(I_y*I_x, sigma=1)
+Iyy = ndi.gaussian_filter(I_y**2, sigma=1)
+
+k = 0.2
+# determinant
+detA = Ixx * Iyy - Ixy ** 2
+# trace
+traceA = Ixx + Iyy
+    
+harris_response = detA - k * traceA ** 2
+
+for y in range(offset, height-offset):
+    for x in range(offset, width-offset):
+        Sxx = np.sum(Ixx[y-offset:y+1+offset, x-offset:x+1+offset])
+        Syy = np.sum(Iyy[y-offset:y+1+offset, x-offset:x+1+offset])
+        Sxy = np.sum(Ixy[y-offset:y+1+offset, x-offset:x+1+offset])
+
+
+#Find determinant and trace, use to get corner response
+det = (Sxx * Syy) - (Sxy**2)
+trace = Sxx + Syy
+r = det - k*(trace**2)        
+
+img_copy_for_corners = np.copy(img)
+for rowindex, response in enumerate(harris_response):
+    for colindex, r in enumerate(response):
+        if r > 0:
+            # this is a corner
+            img_copy_for_corners[rowindex, colindex] = [255,0,0]
+
+cv2.imshow('image',img_copy_for_corners)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+cv2.imwrite('out.png',img_copy_for_corners)
